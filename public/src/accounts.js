87e2469d24e23c74e2a7254b03d3ac96d0749699
Kate Pingryn <katepingryn@gmail.com>
2023-11-16 16:43:16 -0600

    Complete qualified assignment,local library

---
+++ public/src/accounts.js
@@ -0,0 +1,46 @@
+function findAccountById(accounts, id) {
+  return accounts.find((accounts) => accounts.id === id) || null;
+}
+
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((a, b) =>
+    a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1
+  );
+}
+
+function getAccountFullNames(accounts) {
+  return accounts.map(
+    (account) => `${account.name.first} ${account.name.last}`
+  );
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
